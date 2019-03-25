5c958bde578f35d3252998b7c53b58d2a7b45270
ByeongGil-Jung <jbkcose@gmail.com>
2019-03-25 15:56:54 +0900

    [19-03-25] Init - Init

---
+++ src/main/java/com/vraptor/vraptorinterpreter/VraptorInterpreterApplication.java
@@ -0,0 +1,12 @@
+package com.vraptor.vraptorinterpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VraptorInterpreterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VraptorInterpreterApplication.class, args);
+    }
+}
